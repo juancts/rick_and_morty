33f392c200586e4e75db95c8fcc034efa6bd6f37
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-03-21 20:20:44 +0100

    deploy

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,47 +1,69 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./ContactForm.css";
+import emailjs from '@emailjs/browser';
+import Swal from "sweetalert2";
 
 function ContactForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = (e) => {
+  const form = useRef();
+  
+  const sendEmail = (e) => {
     e.preventDefault();
-    // TODO: handle form submission
-    console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+
+    emailjs
+      .sendForm(
+        "service_kacchp3",
+        "template_u0j4unb",
+        e.target,
+        "zi1Em8ggG9bfQ1sVl"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          Swal.fire({
+            icon: "success",
+            title: "Message sent sucessfully 👍",
+          });
+        },
+        (error) => {
+          console.log(error.text);
+          Swal.fire({
+            icon: "error",
+            title: "Something went wrong 🙃",
+            text: error.text,
+          });
+        }
+      );
+    e.target.reset();
   };
+
 
   return (
     <div className="form-container">
         <h1>PLEASE, FEEL FREE TO CONTACT ME:</h1>
-      <form onSubmit={handleSubmit}>
+      <form ref={form} onSubmit={sendEmail}>
         <h2>
           <label htmlFor="name">Name:</label>
         </h2>
 
         <input
           type="text"
+          name="user_name"
           id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
           required
         />
 
         <label htmlFor="email"><h2>Email:</h2></label>
         <input
           type="email"
+          name="from_email"
           id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
           required
         />
 
         <label htmlFor="message"><h2>Message:</h2></label>
         <textarea
           id="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          name="message"
           required
         />
 
