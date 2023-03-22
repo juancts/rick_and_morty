c4d5feb73af42809ca627874a56e788a7445ea39
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-03-22 17:28:14 +0100

    orden de cards!

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,31 +1,33 @@
 import Card from "../Card/Card";
-import "./cards.css"
-
+import SearchBar from "../SearchBar/SearchBar";
+import "./cards.css";
 
 export default function Cards(props) {
   const { characters } = props;
   return (
-    <div className="cards-container" >
-      <div>
+    <div className="title">
+      <div color="white">
         <h1>WELCOME TO THE ULTRA</h1>
         <h1>MEGA RICK & MORTY APP</h1>
+        <p>¡Add a card and se its details!</p>
       </div>
-      <div style={{
-        display: "flex",
-        justifyContent: "center",
-      }}>
-      {characters.map((character) => (
-        <Card
-          detailId={character.id}
-          key={character.name}
-          name={character.name}
-          species={character.species}
-          gender={character.gender}
-          image={character.image}
-          onClose={() => props.onClose(character.id)}
-        />
-      ))}
-
+      <div>
+      <SearchBar onSearch={props.onSearch} />
+      {console.log("PROPS ON SEARCH:", props.onSearch)}
+      </div>
+      <div className="cards-container">
+        {characters.map((character) => (
+          <Card
+            className="card"
+            detailId={character.id}
+            key={character.name}
+            name={character.name}
+            species={character.species}
+            gender={character.gender}
+            image={character.image}
+            onClose={() => props.onClose(character.id)}
+          />
+        ))}
       </div>
     </div>
   );
